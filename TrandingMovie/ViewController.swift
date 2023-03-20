57ce9ec7c5513afe94c8cecd8e48ff5a0f63be1f
Artem <artem-orlov92@bk.ru>
2023-03-20 21:29:54 +0300

    Initial Commit

---
+++ TrandingMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrandingMovie
+//
+//  Created by Артем Орлов on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
